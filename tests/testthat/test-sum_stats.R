d01b3e9936e9435c09b0ddfae331932499b13be6
Jacob <jacob_519@hotmail.com>
2021-11-20 13:57:10 -0800

    updated tests

--- tests/testthat/test-sum_stats.R
+++ tests/testthat/test-sum_stats.R
@@ -11,10 +11,14 @@ test_that("the code outputs a string", {
 test_that("the code produces an error for non numeric argument", {
   expect_error(sum_stats(gapminder::gapminder, gapminder::gapminder$country))
 })
-
+# test that the function output matches what we'd expect
+test_that("the code outputs a string", {
+  expect_output((sum_stats(gapminder::gapminder, gapminder::gapminder$lifeExp)), 
+                regexp="For the provided data, the mean is: 59.47 the median is: 60.71 , the interquartile range is: 22.65 , and the standard deviation is: 12.92 . Plots generated. Check them out to view the distribution of the variable of interest")
+})
+# test that the function output matches what we'd expect
 test_that("the code outputs a string", {
   expect_output(sum_stats(palmerpenguins::penguins, palmerpenguins::penguins$bill_length_mm), 
                 regexp="For the provided data, the mean is: 43.92 the median is: 44.45 , the interquartile range is: 9.27 , and the standard deviation is: 5.46 . Plots generated. Check them out to view the distribution of the variable of interest")
 })
-
 
